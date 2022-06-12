1ec580677dc5d3ac2ea73e4f5fe479bb36289764
Daniel <danielivam96@gmail.com>
2022-06-12 16:42:18 -0300

    Frutas

---
+++ 14) Frutas.js
@@ -0,0 +1,13 @@
+let fruta = "Maçã"
+
+switch (fruta) {
+    case "Maçã":
+        console.log("Não vendemos esta fruta aqui")
+    break
+    case "Kiwi":
+        console.log("Estamos com escassez de kiwis")
+    break
+    case "Melancia":
+        console.log("Aqui está, são 3 reais o quilo")
+    break
+}
---
+++ 15) Carros.js
@@ -0,0 +1,17 @@
+tipo = "Caminhonete"
+switch (tipo) {
+    case "Hatch":
+    console.log("Compra efetuada com sucesso!")
+    break
+    case "Sedan":
+    console.log("Tem certeza que prefere esse modelo?")
+    break
+    case "Motocicleta":
+    console.log("Tem certeza que prefere esse modelo?")
+    break
+    case "Caminhonete":
+    console.log("Tem certeza que prefere esse modelo?")
+    break
+    default:
+    console.log("Não trabalhamos com este tipo de automovel aqui!")
+}
