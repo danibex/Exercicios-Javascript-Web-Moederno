4918c9e59ac7db5cb53a2e78c2fd396a8f3bb2c8
Daniel <danielivam96@gmail.com>
2022-06-12 23:15:13 -0300

    Create 18) Por extenso.js

---
+++ 18) Por extenso.js
@@ -0,0 +1,42 @@
+let n = 1
+function porExtenso(number) {
+    switch (number) {
+        case 0:
+            return `Zero`
+        break
+        case 1:
+            return `Um`
+        break
+        case 2:
+            return `Dois`
+        break
+        case 3:
+            return `Três`
+        break
+        case 4:
+            return `Quatro`
+        break
+        case 5:
+            return `Cinco`
+        break
+        case 6:
+            return `Seis`
+        break
+        case 7:
+            return `Sete`
+        break
+        case 8:
+            return `Oito`
+        break
+        case 9:
+            return `Nove`
+        break
+        case 10:
+            return `Dez`
+        break
+        default:
+            return `Número fora do intervalo`
+    }
+}
+
+console.log(porExtenso(n))
